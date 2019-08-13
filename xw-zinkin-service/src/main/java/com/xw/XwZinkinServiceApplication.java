8e0b8bb189ba83bf3a2bc1a7296649a790232228
wangzhanyang <1026533631@qq.com>
2019-08-13 17:23:43 +0800

    11

---
+++ xw-zinkin-service/src/main/java/com/xw/XwZinkinServiceApplication.java
@@ -0,0 +1,13 @@
+package com.xw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XwZinkinServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XwZinkinServiceApplication.class, args);
+    }
+
+}
