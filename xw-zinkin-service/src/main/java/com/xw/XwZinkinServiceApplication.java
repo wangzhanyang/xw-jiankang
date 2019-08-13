7dbdda5e36b974cae4bbe0706497bd21fdf0a8f6
wangzhanyang <1026533631@qq.com>
2019-08-13 17:31:10 +0800

    11

--- xw-zinkin-service/src/main/java/com/xw/XwZinkinServiceApplication.java
+++ xw-zinkin-service/src/main/java/com/xw/XwZinkinServiceApplication.java
@@ -2,8 +2,10 @@ package com.xw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
 @SpringBootApplication
+@EnableZipkinStreamServer
 public class XwZinkinServiceApplication {
 
     public static void main(String[] args) {
